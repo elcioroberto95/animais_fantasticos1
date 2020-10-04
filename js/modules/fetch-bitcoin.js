8e62ec9cd8a6634f13fffafeda94090531fb398c
Elcio <elcioroberto1995@gmail.com>
2020-10-04 16:39:49 -0300

    curso origamid

---
+++ js/modules/fetch-bitcoin.js
@@ -0,0 +1,17 @@
+export default function initFetchBitcoin(){
+
+    fetch(`https://blockchain.info/ticker`)
+    .then(response => response.json())
+    .then (bitcoin =>{
+    const btcPreco = document.querySelector('.btc-preco');
+    btcPreco.innerText = (1000 / bitcoin.BRL.sell).toFixed(4) 
+    }).catch(erro =>{
+        console.error(erro)
+    })
+    
+}
+
+
+
+
+//https://blockchain.info/ticker
